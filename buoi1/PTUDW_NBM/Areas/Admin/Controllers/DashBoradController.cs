78951c9681a1b9f0fac68334c7c95ec77a6159bc
NguyenBaoMy <my.nb.62cntt@ntu.edu.vn>
2023-09-23 20:01:01 +0700

    buoi1

---
+++ buoi1/PTUDW_NBM/Areas/Admin/Controllers/DashBoradController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PTUDW_NBM.Areas.Admin.Controllers
+{
+    public class DashBoradController : Controller
+    {
+        // GET: Admin/DashBorad
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ buoi1/PTUDW_NBM/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PTUDW_NBM.MvcApplication" Language="C#" %>
