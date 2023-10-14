36c1727158e0bb4a7a7a5c06dd793d20150ba930
NguyenBaoMy <my.nb.62cntt@ntu.edu.vn>
2023-10-14 15:41:43 +0700

    buoi3

---
+++ buoi1/MyClass/Model/Categories.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyClass.Model
+{
+    [Table("Categories")]
+    public class Categories
+    {
+        [Key] public int Id { get; set; }
+        [Required][Display (Name="Tên loại SP")] public string Name { get; set; }
+        [Display(Name = "Tên rút gọn")] public string Slug { get; set; }
+        [Display(Name = "Cấp cho")] public int? ParentID { get; set; }
+        [Display(Name = "Sắp xếp")] public int? Order { get; set; }
+        [Required] [Display(Name = "Mô tả")] public string MetaDesc { get; set; }
+        [Required] [Display(Name = "Từ khoá")] public string MetaKey { get; set; }
+        [Required] [Display(Name = "Ngày tạo")] public DateTime CreateAt { get; set; }
+        [Required] [Display(Name = "Tạo bởi")] public int CreateBy { get; set; }
+        [Required] [Display(Name = "Ngày cập nhật")] public DateTime UpdateAt { get; set; }
+        [Required] [Display(Name = "Cập nhật bởi")] public int UpdateBy { get; set; }
+        [Required] [Display(Name = "Trạng thái")] public int Status { get; set; }
+    }
+}
+
---
+++ buoi1/PTUDW_NBM/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,105 @@
+﻿using System.Data.Entity;
+using System.Net;
+using System.Web.Mvc;
+using MyClass.Model;
+using MyClass.DAO;
+
+namespace PTUDW_NBM.Areas.Admin.Controllers
+{
+    public class CategoryController : Controller
+    {
+        CategoriesDAO categoriesDao = new CategoriesDAO();
+
+        // GET: Admin/Category
+        public ActionResult Index()
+        {
+            return View(categoriesDao.getList("Index"));
+        }
+
+        // GET: Admin/Category/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Categories categories = categoriesDao.getRow(id);
+            if (categories == null)
+            {
+                return HttpNotFound();
+            }
+            return View(categories);
+        }
+
+        // GET: Admin/Category/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Categories categories)
+        {
+            if (ModelState.IsValid)
+            {
+                categoriesDao.Insert(categories);
+                return RedirectToAction("Index");
+            }
+
+            return View(categories);
+        }
+
+        // GET: Admin/Category/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Categories categories = categoriesDao.getRow(id);
+            if (categories == null)
+            {
+                return HttpNotFound();
+            }
+            return View(categories);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Categories categories)
+        {
+            if (ModelState.IsValid)
+            {
+                categoriesDao.Update(categories);
+                return RedirectToAction("Index");
+            }
+            return View(categories);
+        }
+
+        // GET: Admin/Category/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Categories categories = categoriesDao.getRow(id);
+            if (categories == null)
+            {
+                return HttpNotFound();
+            }
+            return View(categories);
+        }
+
+        // POST: Admin/Category/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Categories categories = categoriesDao.getRow(id);
+            categoriesDao.Delete(categories);
+            return RedirectToAction("Index");
+        }
+    }
+}
